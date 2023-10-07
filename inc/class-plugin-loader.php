53c040e6a0ce3d58b5b5f32a12d827828d32a938
kjroelke <kj.roelke@gmail.com>
2023-10-07 17:36:06 -0500

    Color Container Block Fixes #1

--- inc/class-plugin-loader.php
+++ inc/class-plugin-loader.php
@@ -10,12 +10,14 @@ class Plugin_Loader {
 	/** Load the Blocks */
 	public function __construct() {
 		add_action( 'init', array( $this, 'register_blocks' ) );
+		add_filter( 'block_categories_all', array( $this, 'k1_block_category' ) );
 	}
 
 	/** Register the blocks */
 	public function register_blocks() {
 		$blocks = array(
 			'hero',
+			'color-reveal-container'
 		);
 
 		foreach ( $blocks as $block ) {
@@ -23,5 +25,23 @@ class Plugin_Loader {
 				PLUGIN_PATH . "build/blocks/{$block}"
 			);
 		}
+	}
+	/** Creates new "K1 Blocks" block category and sets its position to be 3rd in the list
+	 *
+	 * @param array $categories the array of block categories
+	 * @return array the updated categories array
+	 */
+	public function k1_block_category( array $categories ): array {
+		$k1_category = array(
+			'k1' => array(
+				'slug'  => 'k1-blocks',
+				'title' => 'Blocks for K1 Site',
+			),
+		);
+
+		$position   = 0;
+		$categories = array_slice( $categories, 0, $position, true ) + $k1_category + array_slice( $categories, $position, null, true );
+		$categories = array_values( $categories );
+		return $categories;
 	}
 }
