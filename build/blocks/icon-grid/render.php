073a02b4fedcb78e20ab0bca8cc7be75896fad03
kjroelke <kj.roelke@gmail.com>
2023-10-29 16:14:44 -0500

    update icon grid

--- build/blocks/icon-grid/render.php
+++ build/blocks/icon-grid/render.php
@@ -1,12 +1,15 @@
 <?php
-/** The "Community, Tools and Knowledge" Icon Grid */
+/** The "Community, Tools and Knowledge" Icon Grid
+ *
+ * @package KingdomOne
+ */
 
 ?>
 <section class="community">
 	<div class="container">
 		<div class="row">
 			<h2 class="headline mt-5 text-white">Community, Tools &amp; Knowledge</h2>
-			<?php k1_get_svg_asset( 'leaves-4' ); ?>
+			<?php k1_the_leaf( 4, 'var(--wp--preset--color--primary)', 'right' ); ?>
 		</div>
 		<div class="row">
 			<div class="col-lg-6 subheadline">Our Tools, Training & Talent deliver the expertise you need to develop a healthy ministry.</div>
@@ -18,50 +21,60 @@
 				<?php
 					$grid_items = array(
 						array(
-							'svg'  => 'compass',
-							'text' => 'Expert Guidance with a Kingdom One Ministry Partner',
+							'svg'   => 'hr',
+							'title' => 'HR',
+							'text'  => array( 'Staffing', 'Handbooks', 'Payroll', 'Ministry Cohorts' ),
 						),
 						array(
-							'svg'  => 'restroom',
-							'text' => 'Sexual Harrassment Prevention Training',
+							'svg'   => 'finance',
+							'title' => 'Finance',
+							'text'  => array( 'Financial Reporting', 'Reconciliation', 'Expense Tracking' ),
 						),
 						array(
-							'svg'  => 'justice',
-							'text' => 'AB-506 Child Safety & Mandated Reporting Compliance Training',
+							'svg'   => 'marcom',
+							'title' => 'Marcom',
+							'text'  => array( 'Grapghic Design', 'Branding', 'Social Media Strategy', 'Video Editing' ),
 						),
 						array(
-							'svg'  => 'git-network',
-							'text' => 'HR Ministry Network Conference Online Course',
+							'svg'   => 'webDev',
+							'title' => 'Web Development',
+							'text'  => array( 'Storybranding', 'Customer Journey', 'Wireframing', 'Web Design & Development' ),
 						),
 						array(
-							'svg'  => 'sitemap',
-							'text' => 'Talent Planning &amp; Organizational Leveling',
+							'svg'   => 'strategy',
+							'title' => 'Strategy',
+							'text'  => array( 'Staff Leveling', 'Communications Frameworks', 'Coaching, Advisory, Strategic Planning' ),
 						),
 						array(
-							'svg'  => 'search-dollar',
-							'text' => 'Annual Compensation Survey Report',
+							'svg'   => 'staffing',
+							'title' => 'Business Administration',
+							'text'  => array( 'ChMS Implementation & Optimization', 'Audit & 990 Support', 'Business Process & Procedure Optimization' ),
 						),
 						array(
-							'svg'  => 'profile',
-							'text' => 'Staffing Searches',
+							'svg'   => 'spark',
+							'title' => 'Spark',
+							'text'  => array( 'Compensation Reporting', 'Benefits', 'Staffing Searches' ),
 						),
 						array(
-							'svg'  => 'medical',
-							'text' => 'Benefits, Insurance &amp; Total Rewards',
+							'svg'   => 'academy',
+							'title' => 'Academy',
+							'text'  => array( 'Trainings', 'Tutorials', 'Templates', 'Tools' ),
 						),
 						array(
-							'svg'  => 'group-add',
-							'text' => 'Ministry Cohorts',
-						),
-						array(
-							'svg'  => 'plus-circle',
-							'text' => 'And so much more...',
+							'svg'   => 'justice',
+							'title' => 'Above Reproach',
+							'text'  => array( 'AB-506 Child Safety Training', 'Harrassment Prevention Training', 'Legal Updates' ),
 						),
 					);
 					foreach ( $grid_items as $item ) {
-						$markup  = "<div class='community__grid--item'><div class='community__grid-item-content'>";
-						$markup .= k1_get_svg_asset( 'front-page-icon-' . $item['svg'], false, false );
-						$markup .= "<p class='community__grid-item-content--text'>{$item['text']}</p>";
+						$markup  = "<div class='community__grid--item text-center'><div class='community__grid-item-content'>";
+						$markup .= get_the_k1_icon( $item['svg'] );
+						$markup .= "<h3 class='community__grid__label'>{$item['title']}</h3>";
+						$markup .= "<ul class='community__grid--list list-unstyled text-center'>";
+						foreach ( $item['text'] as $list_item ) {
+							$markup .= "<li class='community__grid--list-item'>{$list_item}</li>";
+						}
+						$markup .= '</ul>';
 						$markup .= '</div></div>';
 						echo $markup;
 					}
--- inc/k1-leaf-icons.php
+++ inc/k1-leaf-icons.php
@@ -1,22 +1,22 @@
 <?php
 /**
  * K1 Leaf Icons
- * 
+ *
  * @package KingdomOne
  */
 
-/** 
+/**
  * Returns an SVG inside a div
- * 
- * @param string $leaf the leaf variant to return 
+ *
+ * @param int    $leaf the leaf variant to return
  * @param string $color the leaf color
  * @param string $direction the direction of the leaves
  * @return string the markup
-*/
-function k1_get_the_leaf( int $leaf, string $color = 'white', string $direction = 'left' ):string {
-	$scale = ( 'left' === $direction ) ? 1 : -1;
-	$markup = "<div class='k1_leaf__container' id='leaves-{$leaf}' style='color:{$color};scaleX:{$scale}>";
-	switch($leaf) {
+ */
+function k1_get_the_leaf( int $leaf, string $color = '', string $direction = 'left' ): string {
+	$scale  = ( 'left' === $direction ) ? -1 : 1;
+	$markup = "<div class='k1_leaf__container' id='leaves-{$leaf}' style='color:{$color};transform:scaleX({$scale}) scaleY(-1);'>";
+	switch ( $leaf ) {
 		case 2:
 			$markup .= '<svg width="190.716" height="217.758" viewBox="0 0 190.716 217.758"><defs><clipPath id="a"><rect width="147.637" height="185.88" fill="none" stroke="currentColor" stroke-width="1" /></clipPath></defs><g transform="matrix(-0.966, 0.259, -0.259, -0.966, 190.716, 179.547)"><g clip-path="url(#a)"><path d="M328.262,338.491c11.175,24.413-4.392,54.661-4.392,54.661s-35.992-6.646-47.167-31.059c-8.1-17.7-1.54-39.174,2.681-49.644h0l2.865-6.36S317.087,314.078,328.262,338.491Z" transform="translate(-263.177 -213.148)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6" /><path d="M50.641,348.131c-2.718-4.734-6.76-17.435-6.76-17.435s23.866-9.978,38.561-2.763c8.827,4.335,14.331,12.855,17.546,19.866a59.368,59.368,0,0,1,3.875,11.119,62.159,62.159,0,0,1-9.429,3.945c-7.927,2.619-19.478,4.789-29.161.034C58.681,359.661,53.951,353.9,50.641,348.131Z" transform="translate(-188.775 -172.509) rotate(-34)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6"/></g></g></svg>';
 			break;
@@ -27,17 +27,17 @@ function k1_get_the_leaf( int $leaf, string $color = 'white', string $direction 
 			$markup .= '<svg width="147.637" height="185.88" viewBox="0 0 147.637 185.88"><defs><clipPath id="a"><rect width="147.637" height="185.88" fill="none" stroke="currentColor" stroke-width="1" /></clipPath></defs><g clip-path="url(#a)"><path d="M328.262,338.491c11.175,24.413-4.392,54.661-4.392,54.661s-35.992-6.646-47.167-31.059c-8.1-17.7-1.54-39.174,2.681-49.644h0l2.865-6.36S317.087,314.078,328.262,338.491Z" transform="translate(-263.177 -213.148)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6" /><path d="M50.641,348.131c-2.718-4.734-6.76-17.435-6.76-17.435s23.866-9.978,38.561-2.763c8.827,4.335,14.331,12.855,17.546,19.866a59.368,59.368,0,0,1,3.875,11.119,62.159,62.159,0,0,1-9.429,3.945c-7.927,2.619-19.478,4.789-29.161.034C58.681,359.661,53.951,353.9,50.641,348.131Z" transform="translate(29.745 -221.309)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6" /><path d="M32.793,41.793C19.6,39.624,9.875,27.649,5.527,21.229L3,17.157H3S20.913.365,39.113,3.357,68.582,26.821,68.582,26.821,50.993,44.785,32.793,41.793Z" transform="translate(74.56 3.959)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6" /><path d="M48.211,56.7C21.364,57.047,0,30.573,0,30.573S20.638.346,47.485,0C66.952-.246,83.934,14.443,91.8,22.538l4.657,5.193h0S75.058,56.359,48.211,56.7Z" transform="translate(99.954 108.38) rotate(-163)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="6" /></g></svg>';
 			break;
 	}
-	$markup .= "</div>";
+	$markup .= '</div>';
 	return $markup;
 }
 
-/** 
+/**
  * Echoes the value of `k1_get_the_leaft($leaf)`
- * 
- * @param int $leaf the leaf variant to return
+ *
+ * @param int    $leaf the leaf variant to return
  * @param string $color the leaf color
  * @param string $direction the direction of the leaves
-*/
-function k1_the_leaf(int $leaf, string $color = 'white', string $direction = 'left') {
-	echo k1_get_the_leaf( $leaf, $color, $direction);
+ */
+function k1_the_leaf( int $leaf, string $color = 'white', string $direction = 'right' ) {
+	echo k1_get_the_leaf( $leaf, $color, $direction );
 }
